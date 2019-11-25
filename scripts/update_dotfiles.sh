d6015f56fd401e8d3be908a9e17a4b87c16db5c1
MacDesu <32233686+macdesu@users.noreply.github.com>
2019-11-25 16:12:43 +0100

    updated update_dotfiles.sh

--- scripts/update_dotfiles.sh
+++ scripts/update_dotfiles.sh
@@ -4,3 +4,4 @@ cp .zshrc .Xresources .xinitrc .vimrc -t $HOME/code/public/dotfiles/
 cd $HOME/.config/
 cp -Ruv i3/ polybar/ ranger/ redshift.conf -t $HOME/code/public/dotfiles/.config
 echo "Dotfiles repo updated !"
+cd $HOME/code/public/dotfiles && git status
