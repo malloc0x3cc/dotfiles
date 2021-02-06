67b635f67039866f826c946707a1bc8179b355b1
malloc0x3cc <32233686+malloc0x3cc@users.noreply.github.com>
2021-02-06 06:13:34 +0100

    fixed mirrorlist script

--- scripts/pacman_mirrorlist.sh
+++ scripts/pacman_mirrorlist.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-
-COUNTRY="France"
-URL="https://www.archlinux.org/mirrorlist/?country=FR&protocol=http&protocol=https&ip_version=4"
+COUNTRY=FR
+NUMBER=5
+echo "You can find the old list in /etc/pacman.d/mirrorlist.backup"
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl $URL > /etc/pacman.d/mirrorlist.backup
-awk '/^## $(COUNTRY)$/{f=1; next}f==0{next}/^$/{exit}{print substr($0, 1);}' /etc/pacman.d/mirrorlist.backup
-sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
-rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+echo "Fetchning the $NUMBER fastest mirrors in $COUNTRY..."
+curl -s "https://archlinux.org/mirrorlist/?country=$COUNTRY&country=GB&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n $NUMBER - > /etc/pacman.d/mirrorlist
 echo "Mirrorlist updated !"
