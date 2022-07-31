d6b9795c5362c9b7adaeb403ac6e9d650117281c
Mallone Delwaulle <32233686+malloc0x3cc@users.noreply.github.com>
2022-07-31 16:59:28 +0200

    New install, rework in progress

--- scripts/arch_mirrorlist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-COUNTRY=FR
-NUMBER=5
-echo "You can find the old list in /etc/pacman.d/mirrorlist.backup"
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-echo "Fetchning the $NUMBER fastest mirrors in $COUNTRY..."
-curl -s "https://archlinux.org/mirrorlist/?country=$COUNTRY&country=GB&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n $NUMBER - > /etc/pacman.d/mirrorlist
-echo "Mirrorlist updated !"
